49150c38e22b0ecf6845c2f6787413c4362410e4
webspreb <priyeshchaturvedi757@gmail.com>
2023-10-29 03:28:42 +0530

    ver8

--- js/custom.js
+++ js/custom.js
@@ -16,3 +16,34 @@ window.addEventListener('click', function(event) {
         modal.style.display = 'none';
     }
 });
+
+var currentSection = 1;
+var numSections = 4; // Change this if you have more sections
+
+function updateProgressBar() {
+    var width = (currentSection - 1) / numSections * 100;
+    $('.progress-bar').css('width', width + '%');
+}
+
+function nextSection(current) {
+    document.getElementById('section' + current).classList.remove('active');
+    currentSection = current + 1;
+    document.getElementById('section' + currentSection).classList.add('active');
+    updateProgressBar();
+}
+
+function prevSection(current) {
+    document.getElementById('section' + current).classList.remove('active');
+    currentSection = current - 1;
+    document.getElementById('section' + currentSection).classList.add('active');
+    updateProgressBar();
+}
+
+function submitForm() {
+    // Capture the selected option in the current section and store it in the hidden input field
+    var selectedOption = document.querySelector('input[name="radio' + currentSection + '"]:checked');
+    document.getElementById('selectedOption' + currentSection).value = selectedOption ? selectedOption.value : '';
+
+    // Submit the form
+    document.getElementById('multiSectionForm').submit();
+}
---
+++ process_form.php
@@ -0,0 +1,35 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the data from the form
+    $name = $_POST["name"];
+    $phoneNumber = $_POST["phoneNumber"];
+    $email = $_POST["email"];
+    $selectedOption1 = $_POST["radio1"];
+    $selectedOption2 = $_POST["radio2"];
+    $selectedOption3 = $_POST["radio3"];
+
+    // Compose the email message
+    $message = "Name: $name\n";
+    $message .= "Phone Number: $phoneNumber\n";
+    $message .= "Email: $email\n";
+    $message .= "Selected Option in Section 1: $selectedOption1\n";
+    $message .= "Selected Option in Section 2: $selectedOption2\n";
+    $message .= "Selected Option in Section 3: $selectedOption3\n";
+
+    // Set the recipient email address
+    $to = "hlanjsdzcyniuneica@cazlp.com"; // Change this to your recipient's email address
+
+    // Set the subject of the email
+    $subject = "Form Submission";
+
+    // Send the email
+    if (mail($to, $subject, $message)) {
+        header("Location: thank_you.html"); // Change this to the actual thank you page
+        exit();
+    } else {
+        echo "Error: Unable to send email.";
+    }
+} else {
+    // Handle invalid requests
+    echo "Invalid request";
+}
